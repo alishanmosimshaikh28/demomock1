df9abbe7a5d4b4a2de9c14aac1b54193ef4c5b75
alishanmosimshaikh28 <121955910+alishanmosimshaikh28@users.noreply.github.com>
2023-01-06 15:18:42 +0530

    basic framework
    hi,plz check

---
+++ amezonframework/src/amezonframework/homepage.java
@@ -0,0 +1,10 @@
+package amezonframework;
+
+public class homepage {
+public static void main (String args[]) {
+		
+		System.out.println("this is my homepage page");}
+	
+	
+	
+}
---
+++ amezonframework/src/amezonframework/loginpage.java
@@ -0,0 +1,10 @@
+package amezonframework;
+
+public class loginpage {
+	public static void main (String args[]) {
+		
+		System.out.println("this is my login page");
+		System.out.println("this is my login page2");
+	}
+
+}
